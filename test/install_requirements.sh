687f410772363bcb9bdc2fe7ce885bd0d35681e6
Marc Popp <marc.popp@sunny-computing.de>
2017-08-30 09:38:40 +0200

    Smarter dependency handling

---
+++ test/install_requirements.sh
@@ -0,0 +1,15 @@
+DEPENDENCIES="
+	Moose
+	MooseX::NonMoose
+	XML::Generator
+	TAP::Formatter::JUnit
+	Clone
+	TAP::Harness
+	TAP::Formatter::Console
+"
+
+for dep in $DEPENDENCIES; do
+	echo $dep
+	perl -M$dep -e '' && echo "$dep already installed" || cpan -i $dep || exit $?
+done
+
