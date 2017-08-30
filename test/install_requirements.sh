8caf273bc4abfbe6449d7725d3514c395e3dd617
Marc Popp <marc.popp@sunny-computing.de>
2017-08-30 10:23:44 +0200

    Dependency handling of cpan sucks: adding List::MoreUtils:XS

--- test/install_requirements.sh
+++ test/install_requirements.sh
@@ -1,6 +1,7 @@
 DEPENDENCIES="
 	Moose
 	List::MoreUtils
+	List::MoreUtils:XS
 	MooseX::NonMoose
 	XML::Generator
 	TAP::Formatter::JUnit
@@ -10,7 +11,10 @@ DEPENDENCIES="
 "
 
 for dep in $DEPENDENCIES; do
+	echo
+	echo
 	echo $dep
 	perl -M$dep -e '' && echo "$dep already installed" || cpan -i $dep || exit $?
+	echo
 done
 
